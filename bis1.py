01744d166919fa261f9bb5120bbf6d6e40f22b20
Bisan <beesan2012002@gmail.com>
2022-09-24 15:16:23 +0300

    loop ass 2

--- bis1.py
+++ bis1.py
@@ -1,4 +1,12 @@
-for i in range(5):
-    print(i)
-else:
-    print("Done!")
+numbers = [12, 75, 150, 180, 145, 525, 50]
+result_num = []
+for item in list(numbers):
+    if item > 500:
+        break
+
+    if item > 150:
+        continue
+
+    if item % 5 == 0:
+        result_num.append(item)
+print(result_num)
