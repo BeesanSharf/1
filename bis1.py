9350ca131fb558155b4529432328841a4bd21a21
Bisan <beesan2012002@gmail.com>
2022-09-24 15:10:34 +0300

    loop ass Display numbers from a list using a loop

--- bis1.py
+++ bis1.py
@@ -1,29 +1,12 @@
-numbers = [12,75,150,180,145,525,50]
+numbers = [12, 75, 150, 180, 145, 525, 50]
 result_num = []
 for item in list(numbers):
-   if item > 500:
-       break
+    if item > 500:
+        break
 
-   if item > 150:
-       continue
+    if item > 150:
+        continue
 
-   if item % 5 == 0 :
-       result_num.append(item)
+    if item % 5 == 0:
+        result_num.append(item)
 print(result_num)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
